c31418eb0c7d3b83d92d92adcc9232d7126fc175
firman ramadhan <firmanramadhan066@gmail.com>
2024-10-16 11:32:49 +0700

    update

--- tambah_barang.php
+++ tambah_barang.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Barang</title>
-    <link rel="stylesheet" href="../inventory_management/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
